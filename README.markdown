eca47a295c493f8c0f1e12e9b6631152fdf2f13f
Ericka Ceron <erickaecv@yahoo.com.mx>
2011-06-14 10:45:07 -0500

    Commit inicial

---
+++ README.markdown
@@ -0,0 +1,3 @@
+#  Ruby on Rails Tutorial:Sample App
+
+Este es sample_app[*Ruby on Rails Tutorial:Learn Rauls by Example*] (http://railstutorial.orf/) por [*ECV*] <erickaecv@yahoo.com.mx>.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9a884f33acdc5be4476a10c686ffffd77ec1b38890b22cb5cca980bec7b33578bed75c899bbabeff84bbb72ef8ce7d34b40e9760be13e93a6a92e7c2d3e7b84a'
